34f982f076ebaee0dc161d041cb86177923de4e3
ashu7354848036-gif <ashu7354848036@gmail.com>
2026-03-16 09:06:08 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountID = 178888
+let accountEmail = "ashu@.gmail.com"
+var accountPassword = "12345"
+accountCity = "jabalpur"
+let accountState;
+
+// accountID = 2 // not allowed
+
+accountEmail = "as@hu.com"
+accountPassword = "5423187"
+accountCity = "khurai"
+
+
+console.log(accountID);
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+// "use strict"; treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+console.log(3
+     + 3) // code readability should be high  
+
+
+console.log("ashu")
+
+
+let name = "ashu"
+let age = 27
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// Bighint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object 
+
